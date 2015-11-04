52f22ddac11c9cccd569444d75b9b075e75cb66e
csurof <csurof@gmail.com>
2015-11-04 06:07:54 +0000

    F01

---
+++ csserver.js
@@ -0,0 +1,76 @@
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)  
+
+io.configure(function() {
+    io.set('transports', [ 'websocket' ]);
+    if (process.env.IISNODE_VERSION) {
+        // If this node.js application is hosted in IIS, assume it is hosted 
+        // in IIS virtual directory named 'dante' and set up the socket.io's resource
+        // value for socket.io to recognize requests that target it. 
+        // Note a corresponding change in the client index-socketio.html, as well
+        // as necessary URL rewrite rule in web.config. 
+
+        io.set('resource', '/socket.io');
+    }
+});
+
+function handler (req, res) {
+	
+   /*  fs.readFile(path.resolve(__dirname, 'index-socketio.html'),
+        function (err, data) {
+            if (err) {
+              res.writeHead(500);
+              return res.end('Error loading index-socketio.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        }
+    ); */
+}
+
+io.sockets.on('connection', function (socket) {
+  io.sockets.emit('this', { will: 'be received by everyone'});
+
+  socket.on('private message', function (msg) {
+    //console.log('New Chat Message ', msg);
+     io.sockets.emit('txt',msg); 
+  });
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('User Disconnected');
+  });
+  
+  socket.on('newuser', function (name) {
+    //console.log(name,' Is Now Connected!');
+     io.sockets.emit('txt',name + ' is now online'); 
+  });
+
+  socket.on('exit', function (name) {
+   // console.log(name,' Has Been Disconnected!');
+     io.sockets.emit('txt',name + ' is now offline'); 
+  });
+  
+});
+
+/* io.sockets.on('connection', function (socket) {
+    function schedule(line) {
+        if (line < canto1.length) 
+            setTimeout(function() {
+                if (socket) {
+                    socket.emit('divinecomedy', canto1[line]);
+                    schedule(line + 1);
+                }
+            }, 2000);
+        else if (socket) {
+            socket.disconnect();
+            socket = null;
+        }
+    }
+
+    socket.emit('divinecomedy', canto1[0]);
+    schedule(1);
+}); */
+
+app.listen(process.env.PORT || 80);
